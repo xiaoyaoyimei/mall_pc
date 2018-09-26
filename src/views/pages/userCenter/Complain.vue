489feef115dc69ebf619e303d47c935f46e24554
m-liushikun <1300525832@qq.com>
2018-09-26 16:21:37 +0800

    评价和售后

--- src/views/pages/userCenter/Complain.vue
+++ src/views/pages/userCenter/Complain.vue
@@ -1,6 +1,6 @@
 <template>
 	<div style="padding:40px;">
-		
+		<h3>投诉建议 <i>+</i></h3>
 	</div>
 </template>
 
@@ -19,5 +19,25 @@
 </script>
 
 <style scoped="scoped">
-
+    h3{
+        font-weight: 400;
+        color: #333333;        
+        font-size: 24px;
+        text-align: left;
+    }
+    h3 i{
+        float: right;
+        background-color: #c6c6c6;
+        width: 33px;
+        height: 33px;
+        margin: 0 auto;
+        border-radius: 50%;
+        text-align: center;
+        line-height: 28px;
+        font-weight: 700;
+        font-style: normal;
+        font-size: 30px;
+        color: #FFFFFF;
+        cursor: pointer;
+    }
 </style>
